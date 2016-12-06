b295cd20e78ba0b0e75446a9cf29573cea0866ee
Jinadal <jinadalsanchez@gmail.com>
2016-12-06 11:47:39 +0100

    Servidor

--- src/ServidorFTP.java
+++ src/ServidorFTP.java
@@ -1,4 +1,20 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class ServidorFTP {
+
+
+	public static void main(String args[]) throws Exception
+	{
+		ServerSocket soc=new ServerSocket(5217);
+		System.out.println("Servidor FTP comenzo en el puerto numero 5217");
+		while(true)
+		{
+			System.out.println("Esperando para conectar ...");
+			DataTransfer t=new DataTransfer(soc.accept());
+
+		}
+	}
 
 }
